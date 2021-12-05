999b9cf9fdbc1bc5ad0b1a34cf24238a1b24cd1c
Gaboubou <gab-attal@hotmail.fr>
2021-12-05 23:23:51 +0100

    enfiiiin

--- day03/index.ts
+++ day03/index.ts
@@ -30,42 +30,43 @@ function run1(input: Array<string>) {
     console.log(powerConsumption)
 }
 
-function calculateRating(input: Array<string>, value1: string, value2: string) {
+function getOxygenRating(input: Array<string>) {
     let splitArray: Array<string> = new Array<string>(input[0].length);
     let arrayCounter: Array<number> = new Array<number>(input[0].length);
-    let rating: Array<string> = input;
+    let rating: Array<string> = [...input];
     let index = 0;
     let valueToRemove;
     let lengthRating;
-    let power = splitArray.length - 1;
     let totalRate = 0;
 
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < rating.length; i++) {
         arrayCounter[i] = 0;
     }
 
-    do {
+    while (rating.length > 1) {
         for (let i = 0; i < rating.length; i++) {
             splitArray = rating[i].split("");
             if (splitArray[index] === '1') {
                 arrayCounter[index] += 1;
             }
         }
+        console.log("index :"+index+ "arrayCounter :"+arrayCounter[index])
 
         lengthRating = rating.length;
+        console.log(rating)
 
         for (let i = 0; i < rating.length; i++) {
             splitArray = rating[i].split("");
 
 
             if (arrayCounter[index] >= lengthRating / 2) {
-                if (splitArray[index] !== value1) {
+                if (splitArray[index] !== '1') {
                     valueToRemove = rating.indexOf(rating[i]);
                     rating.splice(valueToRemove, 1);
                     i--;
                 }
             } else {
-                if (splitArray[index] !== value2) {
+                if (splitArray[index] !== '0') {
                     valueToRemove = rating.indexOf(rating[i]);
                     rating.splice(valueToRemove, 1);
                     i--;
@@ -74,22 +75,71 @@ function calculateRating(input: Array<string>, value1: string, value2: string) {
 
         }
         index++;
-    } while (rating.length > 1)
-
-    for (let j = 0; j < splitArray.length; j++) {
-        if (splitArray[j]==='1') {
-            totalRate += Math.pow(2, (power))
-        }
-        power--;
     }
+    console.log(rating)
+
+    totalRate = parseInt(rating[0], 2)
     console.log(totalRate)
+    return totalRate
 }
 
+function getC02Rating(input: Array<string>) {
+    let splitArray: Array<string> = new Array<string>(input[0].length);
+    let arrayCounter: Array<number> = new Array<number>(input[0].length);
+    let rating: Array<string> = [...input];
+    let index = 0;
+    let valueToRemove;
+    let lengthRating;
+    let totalRate = 0;
+
+    for (let i = 0; i < rating.length; i++) {
+        arrayCounter[i] = 0;
+    }
+
+    while (rating.length > 1) {
+        for (let i = 0; i < rating.length; i++) {
+            splitArray = rating[i].split("");
+            if (splitArray[index] === '1') {
+                arrayCounter[index] += 1;
+            }
+        }
+        console.log("index :"+index+ "arrayCounter :"+arrayCounter[index])
+
+        lengthRating = rating.length;
+        console.log(rating)
+
+        for (let i = 0; i < rating.length; i++) {
+            splitArray = rating[i].split("");
+
+
+            if (arrayCounter[index] < lengthRating / 2) {
+                if (splitArray[index] !== '1') {
+                    valueToRemove = rating.indexOf(rating[i]);
+                    rating.splice(valueToRemove, 1);
+                    i--;
+                }
+            } else {
+                if (splitArray[index] !== '0') {
+                    valueToRemove = rating.indexOf(rating[i]);
+                    rating.splice(valueToRemove, 1);
+                    i--;
+                }
+            }
+
+        }
+        index++;
+    }
+    console.log(rating)
+    totalRate = parseInt(rating[0], 2)
+    console.log(totalRate)
+    return totalRate
+}
 
 function run2(input: Array<string>) {
-
-    //calculateRating(input, '1', '0')
-    calculateRating(input, '0', '1')
+    let oxygenRating = getOxygenRating(input);
+    let CO2Rating = getC02Rating(input)
+    let finalResult = oxygenRating * CO2Rating;
+    console.log(finalResult);
 }
 
 
